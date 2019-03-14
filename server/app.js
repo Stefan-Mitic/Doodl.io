946851e28a4858bdc3082a16cc1602a35e0512eb
AlexeiCoreiba <alexei.coreiba@gmail.com>
2019-03-13 22:06:21 -0400

    Moved server.js code to app.js

--- server/app.js
+++ server/app.js
@@ -1,21 +1,20 @@
 /* jshint node: true */
-"use strict";
+'use strict';
 
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 const bodyParser = require('body-parser');
-const express = require("express");
+const express = require('express');
 const app = express();
+const socketIO = require('socket.io');
 
 // middleware dependencies
 const validator = require('./middlewares/validation');
 const auth = require('./middlewares/authentication');
 
-// middleware module to handle multipart/form-data 
-const multer  = require('multer');
-let upload = multer({
-    dest: path.join(__dirname, 'assets')
-});
+// middleware module to handle multipart/form-data
+const multer = require('multer');
+let upload = multer({ dest: path.join(__dirname, 'assets') });
 
 // setup global middlewares
 app.use(bodyParser.json());
@@ -39,7 +38,8 @@ const gameImages = require('./controllers/images');
 
 // user authentication routes
 app.post('/login', validator.checkUsername, users.login);
-app.post('/signup', validator.checkUsername, validator.checkPassword, users.signup);
+app.post(
+    '/signup', validator.checkUsername, validator.checkPassword, users.signup);
 app.get('/logout', auth.isAuthenticated, users.logout);
 app.get('/api/users', users.getUsers);
 
@@ -54,7 +54,44 @@ app.delete('/api/game/images/:id/', validator.checkId, gameImages.deleteImage);
 const http = require('http');
 const PORT = 3000;
 const server = http.createServer(app);
-server.listen(PORT, function (err) {
-    if (err) console.log(err);
-    else console.log("HTTP server on https://localhost:%s", PORT);
+const io = socketIO(server);
+
+// SOCKETIO ===================================================================
+
+let lobbies = {};
+
+io.on('connection', socket => {
+    console.log('User connected');
+    let sessionid = socket.id;
+
+    socket.on('join', (params, callback) => {
+        // username and room id
+        if (!isRealString(params.user) || !isRealString(params.gameId)) {
+            callback('Username and game id are required.');
+        }
+
+        socket.emit(
+            'newMessage', generateMessage('Admin', 'Successfully joined game.'));
+        socket.broadcast.to(params.room)
+            .emit(
+                'newMessage',
+                generateMessage('Admin', `${params.user} has joined.`));
+        callback();
+    });
+
+    socket.on('createMessage', (message, callback) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', generateMessage(message.from, message.text));
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(PORT, function(err) {
+    if (err)
+        console.log(err);
+    else
+        console.log('HTTP server on https://localhost:%s', PORT);
 });
