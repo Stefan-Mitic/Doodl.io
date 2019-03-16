1d29adc5f1ccf8bbad1244bf61bb496f8571d2cc
Stefan Mitic <stefan.mitic@mail.utoronto.ca>
2019-03-16 11:32:16 -0400

    updated postgame frontend

--- client/src/pages/PostGame.jsx
+++ client/src/pages/PostGame.jsx
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 
 class PostGame extends Component {
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    getResults() {
+        var rows = [];
+        var players = ['Player 1', 'Player 2', 'Player 3']; // API CALL
+        players.forEach(function (player) {
+            rows.push(<tr key={player}><td>{player}</td><td>0</td></tr>);
+        });
+        return (
+            <tbody>{rows}</tbody>
+        );
     }
 
     render() {
         return (
             <div>
-                <h1>Leaderboard</h1>
+                <Header></Header>
+                <div className="title">Results</div>
+                <div className="row">
+                    <table id="resultsTable" className="offset-sm-2 col-sm-3 table table-bordered table-dark">
+                        <thead>
+                            <tr>
+                                <th>Player</th>
+                                <th>Score</th>
+                            </tr>
+                        </thead>
+                        {this.getResults()}
+                    </table>
+                </div>
+                <div className="row offset-sm-8">
+                    <Link to="/">
+                    <button type="button" className="btn btn-success">Return to Main Menu</button>
+                    </Link>
+                </div>
             </div>
         );
     }
