ab56aab674eea4e52c78dafe86a571741ffabffd
Stefan Mitic <stefan.mitic@mail.utoronto.ca>
2019-03-17 14:10:48 -0400

    added host options for start game

--- client/src/pages/Lobby.jsx
+++ client/src/pages/Lobby.jsx
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import api from '../api';
+import history from '../history';
 import Header from '../components/Header';
 
 class Lobby extends Component {
     constructor(props) {
         super(props);
-        this.state = { show : false };
+        this.state = { show: false };
         this.urlRef = React.createRef();
         this.getPlayers = this.getPlayers.bind(this);
         this.copyText = this.copyText.bind(this);
+        this.host = this.props.location.state.host;
+        this.gameId = this.props.match.params.id;
+        this.startGame = this.startGame.bind(this);
     }
 
     getPlayers() {
         var rows = [];
-        var players = ['Player 1', 'Player 2', 'Player 3']; // API CALL
+        var players = [];
+        api.get(`/api/game/players/`, { id: this.gameId })
+            .then(res => {
+                console.log(res);
+                players = res;
+            }).catch(err => {
+                console.log(err);
+            });
         players.forEach(function (player) {
             rows.push(<tr key={player}><td>{player}</td></tr>);
         });
@@ -26,7 +37,20 @@ class Lobby extends Component {
         var copyText = this.urlRef.current;
         copyText.select();
         document.execCommand('copy');
-        this.setState({ show : true } );
+        this.setState({ show: true });
+    }
+
+    startGame = event => {
+        event.preventDefault();
+
+        api.post(`/api/game/start/`, { id: this.gameId })
+            .then(res => {
+                console.log(res);
+                // socket emit code here
+                history.push({ pathname: "/game/" + this.gameId});
+            }).catch(err => {
+                console.log(err);
+            });
     }
 
     render() {
@@ -43,21 +67,22 @@ class Lobby extends Component {
                         </thead>
                         {this.getPlayers()}
                     </table>
-                    <div className="offset-sm-1 col-sm-4">
-                        <div>Share this link:</div>
-                        <input ref={this.urlRef} type="text" readOnly value="url.com"></input>
-                        <button onClick={this.copyText}>Copy text</button>
+                    <div className="offset-sm-1 col-sm-5">
+                        <div>Share this ID:</div>
+                        <input ref={this.urlRef} type="text" readOnly value={this.gameId}></input>
+                        <button className="btn btn-success" onClick={this.copyText}>Copy text</button>
                         {
                             this.state.show ?
-                            <div className="alert alert-primary" role="alert">Copied!</div>
-                            :null
+                                <div className="alert alert-primary" role="alert">Copied!</div>
+                                : null
                         }
                     </div>
                 </div>
-                <div className="row offset-sm-8">
-                    <Link to="/game">
-                    <button type="button" className="btn btn-success">Start Game</button>
-                    </Link>
+                <div className="row">
+                    <div className="offset-sm-6 col-sm-3" style={{visibility: !this.host ? 'visible' : 'hidden' }}>
+                        Cannot start, not host!
+                    </div>
+                    <button type="button" className="col-sm-2 btn btn-success" disabled={!this.host} onClick={this.startGame}>Start Game</button>
                 </div>
             </div>
         );
