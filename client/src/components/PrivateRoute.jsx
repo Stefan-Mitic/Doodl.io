c691c91fd1459249758332dac3b6ea2dad636634
Stefan Mitic <stefan.mitic@mail.utoronto.ca>
2019-03-16 22:33:50 -0400

    updated with localStorage data

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import Home from '../pages/Home';
 
-let getUsername = function(){
-    return document.cookie.replace(/(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-};
+// let getUsername = function(){
+//     return document.cookie.replace(/(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+// };
 
 export const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route {...rest} render={props => (
-        getUsername()
+        localStorage.getItem('data')
             ? <Home />
             : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
     )} />
