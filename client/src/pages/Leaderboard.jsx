5da117013d81c167a73ab7befe4c3f3b29f1c655
Stefan Mitic <stefan.mitic@mail.utoronto.ca>
2019-03-22 19:11:44 -0400

    implemented leaderboard ui and api calls

--- client/src/pages/Leaderboard.jsx
+++ client/src/pages/Leaderboard.jsx
@@ -1,13 +1,71 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import { getTopPlayers, getPlayerLeaderboard } from '../api';
 
 class Leaderboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { rank: -1, player: null, score: -1, data: [] };
+        this.getPlayerData = this.getPlayerData.bind(this);
+        this.getTopPlayers = this.getTopPlayers.bind(this);
+    }
+
+    componentDidMount() {
+        this.getTopPlayers();
+    }
+
+    getPlayerData() {
+        getPlayerLeaderboard(localStorage.getItem('username'), (res) => {
+            this.setState({ rank: res.data.position, player: res.data.player, score: res.data.score });
+        }, (err) => {
+            alert(err);
+        });
+    }
+
+    getTopPlayers() {
+        getTopPlayers((res) => {
+            const players = [];
+            let i = 1;
+            for (const name of res.data) {
+                const newRecord = { rank: i, name: name };
+                players.push(newRecord);
+                i++;
+            }
+            this.setState({ data: players });
+        }, (err) => {
+            alert(err);
+        });
+    }
 
     render() {
+        const columns = [{
+            Header: 'Ranking',
+            accessor: 'rank'
+        }, {
+            Header: 'Players',
+            accessor: 'name'
+        }]
+
         return (
             <div>
                 <Header></Header>
                 <div className="title">Leaderboard</div>
+                <div className="row">
+                    <div className="player-leaderboard offset-sm-3 col-sm-2">{this.state.rank}</div>
+                    <div className="player-leaderboard col-sm-2">{this.state.player}</div>
+                    <div className="player-leaderboard col-sm-2">{this.state.score}</div>
+                </div>
+                <div className="row">
+                    <ReactTable className="offset-sm-4 col-sm-4 table"
+                        data={this.state.data}
+                        columns={columns}
+                        loadingText={''}
+                        showPagination={false}
+                        defaultPageSize={10}
+                    />
+                </div>
             </div>
         );
     }
