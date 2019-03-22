e5e349d002e87f456eb5f70b9d6c9f86d478edf3
Stefan Mitic <stefan.mitic@mail.utoronto.ca>
2019-03-21 22:12:30 -0400

    added api functions to api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,22 +1,109 @@
 import axios from 'axios';
 import openSocket from "socket.io-client";
+axios.defaults.baseURL = 'http://localhost:5000/';
+
+export function createGame(config, callback, errorcallback) {
+    axios.post('/api/game/', config)
+        .then(res => {
+            if (callback != null) {
+                callback(res);
+            }
+        })
+        .catch(err => {
+            if (errorcallback != null) {
+                errorcallback(err);
+            }
+        });
+}
+
+export function joinGame(config, callback, errorcallback) {
+    axios.patch('/api/game/join/', config)
+        .then(res => {
+            if (callback != null) {
+                callback(res);
+            }
+        })
+        .catch(err => {
+            if (errorcallback != null) {
+                errorcallback(err);
+            }
+        });
+}
+
+export function getPlayers(gameId, callback, errorcallback) {
+    axios.get('/api/game/' + gameId + '/players/')
+        .then(res => {
+            if (callback != null) {
+                callback(res);
+            }
+        })
+        .catch(err => {
+            if (errorcallback != null) {
+                errorcallback(err);
+            }
+        });
+}
+
+export function startGame(gameId, callback, errorcallback) {
+    axios.get('/api/game/start/', { id: gameId })
+        .then(res => {
+            if (callback != null) {
+                callback(res);
+            }
+        })
+        .catch(err => {
+            if (errorcallback != null) {
+                errorcallback(err);
+            }
+        });
+}
+
+export function saveDrawing(config, callback, errorcallback) {
+    axios.post('/api/drawings/', config)
+        .then(res => {
+            if (callback != null) {
+                callback(res);
+            }
+        })
+        .catch(err => {
+            if (errorcallback != null) {
+                errorcallback(err);
+            }
+        });
+}
+
+export function imageCompare(imageId, drawingId, callback, errorcallback) {
+    axios.post('/api/game/images/' + imageId + '/compare/', { drawingId: drawingId })
+        .then(res => {
+            if (callback != null) {
+                callback(res);
+            }
+        })
+        .catch(err => {
+            if (errorcallback != null) {
+                errorcallback(err);
+            }
+        });
+}
+
+// SOCKETIO ===================================================================
 
 const socket = openSocket('http://localhost:5000');
 
-function subscribeToUpdateUserList(updateList) {
-    socket.on('updateUserList', function() {
+export function subscribeToUpdateUserList(updateList) {
+    socket.on('updateUserList', function () {
         updateList();
     });
 }
 
-function subscribeToGameStart(startGame) {
-    socket.on('gameStart', function() {
+export function subscribeToGameStart(startGame) {
+    socket.on('gameStart', function () {
         startGame();
     });
 }
 
-function emitJoin(params) {
-    socket.emit('join', params, function(err) {
+export function emitJoin(params) {
+    socket.emit('join', params, function (err) {
         if (err) {
             console.log(err);
         } else {
@@ -25,7 +112,7 @@ function emitJoin(params) {
     });
 }
 
-function emitStartGame(gameId) {
+export function emitStartGame(gameId) {
     socket.emit('startGame', gameId);
 }
 
@@ -33,5 +120,3 @@ function emitStartGame(gameId) {
 export default axios.create({
     baseURL: `http://localhost:5000/`
 });
-
-export { subscribeToUpdateUserList, subscribeToGameStart, emitJoin, emitStartGame };
