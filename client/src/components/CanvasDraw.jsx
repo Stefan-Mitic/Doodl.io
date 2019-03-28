cb71f7647948da4524e62c9adb5e3863f7392a61
Stefan Mitic <stefan.mitic@mail.utoronto.ca>
2019-03-28 12:06:05 -0400

    attempt at making a grayscale of background img

--- client/src/components/CanvasDraw.jsx
+++ client/src/components/CanvasDraw.jsx
@@ -22,6 +22,12 @@ class CanvasDraw extends Component {
             this.canvas.style.position = 'fixed';
             this.canvas.style.border = '1px solid blue';
             this.canvas.style.backgroundImage = "url(" + this.props.bgsrc + ")";
+
+            // this.loadBackground("url(" + this.props.bgsrc + ")");
+            // this.loadBackground(this.props.bgsrc, function (url) {
+            //     console.log('set');
+            //     // this.canvas.style.backgroundImage = "url(" + url + ")";
+            // });
         }
         this.pos = { x: 0, y: 0 };
 
@@ -29,6 +35,43 @@ class CanvasDraw extends Component {
         ReactDOM.findDOMNode(this).addEventListener('mousedown', this.setPosition);
         ReactDOM.findDOMNode(this).addEventListener('mouseenter', this.setPosition);
     }
+
+    // loadBackground(url) {
+    //     let background = document.createElement('div');
+    //     background.id = 'background'
+    //     background.className = 'overlap';
+    //     background.style.backgroundImage = "url(" + url + ")";
+    //     background.style.backgroundSize = '475px';
+    //     background.style.width = '475px';
+    //     background.style.height = '475px';
+    //     background.style.opacity = 0.5;
+    //     document.getElementById('test').append(background);
+    //     // this.canvas.append(image);
+    //     console.log(document.getElementById('test'));
+    // }
+
+    // loadBackground(url, callback) {
+    //     let image = new Image();
+    //     image.src = url;
+    //     image.crossOrigin = "Anonymous";
+
+    //     let bCanvas = document.createElement('canvas');
+    //     bCanvas.width = 475;
+    //     bCanvas.height = 475;
+
+    //     let bctx = bCanvas.getContext('2d');
+    //     image.onload = function () {
+    //         bctx.drawImage(image, 0, 0);
+    //         let imageData = bctx.getImageData(0, 0, 475, 475);
+    //         for (var i = 0; i < imageData.data.length; i++) {
+    //             if (imageData.data[i] === 0)
+    //                 imageData.data[i] = 150;
+    //         }
+    //         bctx.putImageData(imageData, 0, 0);
+    //         console.log('done');
+    //         callback(bCanvas.toDataURL());
+    //     }
+    // }
 
     setPosition(e) {
         this.pos.x = e.clientX - this.canvas.offsetLeft;
