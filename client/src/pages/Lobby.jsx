095d74af0e88e3493f3c35347ed164b8ef8a7e53
Stefan Mitic <stefan.mitic@mail.utoronto.ca>
2019-03-30 13:35:17 -0400

    added game request functionality to lobby

--- client/src/pages/Lobby.jsx
+++ client/src/pages/Lobby.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { subscribeToUpdateUserList, subscribeToGameStart, emitStartGame, getPlayers, startGame} from '../api';
+import { subscribeToUpdateUserList, subscribeToGameStart, emitStartGame, getPlayers, startGame, getFriends, sendGameRequest } from '../api';
 import history from '../history';
 import Header from '../components/Header';
 import ReactTable from "react-table";
@@ -8,10 +8,12 @@ import "react-table/react-table.css";
 class Lobby extends Component {
     constructor(props) {
         super(props);
-        this.state = { show: false, data: [] };
+        this.state = { show: false, data: [], friends: [] };
         this.urlRef = React.createRef();
         this.getPlayers = this.getPlayers.bind(this);
         this.copyText = this.copyText.bind(this);
+        this.sendRequest = this.sendRequest.bind(this);
+        this.getFriends = this.getFriends.bind(this);
         this.host = this.props.location.state.host;
         this.gameId = this.props.match.params.id;
         this.startGame = this.startGame.bind(this);
@@ -20,6 +22,7 @@ class Lobby extends Component {
 
     componentDidMount() {
         this.getPlayers();
+        this.getFriends();
         subscribeToGameStart(this.redirectToGame);
         subscribeToUpdateUserList(this.getPlayers);
     }
@@ -47,8 +50,8 @@ class Lobby extends Component {
     redirectToGame() {
         startGame(this.gameId, 1, (res) => {
             history.push({
-              pathname: "/game/" + this.gameId,
-              state: { players: this.state.data }
+                pathname: "/game/" + this.gameId,
+                state: { players: this.state.data }
             });
         }, (err) => {
             alert(err);
@@ -61,34 +64,82 @@ class Lobby extends Component {
         this.redirectToGame();
     }
 
+    sendRequest(e, row) {
+        e.preventDefault();
+
+        sendGameRequest(row.name, (res) => {
+            console.log(res);
+            alert('Game Request sent to: ' + row.name);
+        }, (err) => {
+            alert(err);
+        });
+    }
+
+    getFriends() {
+        getFriends(localStorage.getItem('username'), (res) => {
+            console.log(res);
+            const friends = [];
+            for (const friend of res.data) {
+                const newRecord = { name: friend };
+                friends.push(newRecord);
+            }
+            this.setState({ friends: friends });
+        }, (err) => {
+            alert(err);
+        });
+    }
+
     render() {
-        const columns = [{
+        const playerCols = [{
             Header: 'Players',
             accessor: 'name'
-        }]
+        }];
+
+        const friendCols = [{
+            Header: 'Friends List',
+            columns: [{
+                Header: 'Player',
+                accessor: 'name'
+            }, {
+                width: 100,
+                Header: '',
+                style: {
+                    textAlign: 'center',
+                },
+                Cell: ({ row }) => (<button onClick={(e) => this.sendRequest(e, row)}>Invite</button>)
+            }]
+        }];
 
         return (
             <div>
                 <Header></Header>
                 <div className="title">Lobby</div>
                 <div className="row">
-                    <ReactTable className="offset-sm-2 col-sm-3 table"
-                        data={this.state.data}
-                        columns={columns}
-                        loadingText={''}
-                        showPagination={false}
-                        defaultPageSize={4}
-                    />
-
-                    <div className="offset-sm-1 col-sm-5">
-                        <div>Share this ID:</div>
-                        <input ref={this.urlRef} type="text" readOnly value={this.gameId}></input>
+                    <div className="offset-sm-2 col-sm-3">
+                        <ReactTable className="table"
+                            data={this.state.data}
+                            columns={playerCols}
+                            loadingText={''}
+                            showPagination={false}
+                            defaultPageSize={4}
+                        />
+                        <div>OR share this ID:</div>
+                        <input ref={this.urlRef} type="text" size="15" readOnly value={this.gameId}></input>
                         <button className="btn btn-success" onClick={this.copyText}>Copy text</button>
                         {
                             this.state.show ?
                                 <div className="alert alert-primary" role="alert">Copied!</div>
                                 : null
                         }
+                    </div>
+                    <div className="offset-sm-1 col-sm-5">
+                        <ReactTable className="table"
+                            data={this.state.friends}
+                            columns={friendCols}
+                            loadingText={''}
+                            showPagination={true}
+                            defaultPageSize={5}
+                        />
                     </div>
                 </div>
                 <div className="row">
